75a04c6ecf4e786918d40b3091918976bfe1e55b
AJanasik <ajanasik1@gmail.com>
2023-05-04 11:15:53 +0200

    src change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,30 +1,5 @@
-import { galleryItems } from "./gallery-items.js";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
-
-for (const img of galleryItems) {
-  const list = `<div class="gallery__item">
-    <a class="gallery__link" href=${img.original}>
-    <img class="gallery__image" src=${img.preview} data-source=${img.original} alt=${img.description} /> 
-    </a> 
-    </div>`;
-
-  gallery.insertAdjacentHTML("beforeend", list);
-}
-
-gallery.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  if (event.target.tagName !== "IMG") return;
-  const box = basicLightbox.create(
-    `<img src="${event.target.dataset.source}">`
-  );
-  box.show();
-
-  const close = document.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
-      box.close();
-    }
-  });
-});
+console.log(galleryItems);
