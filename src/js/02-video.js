568c704c023b9e383a1d32bf9390063e57a40bb4
AJanasik <ajanasik1@gmail.com>
2023-05-04 12:23:15 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoPlayer = document.querySelector('#vimeo-player');
+const player = new Player(vimeoPlayer);
+
+const playOn = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+
+player.on('timeupdate', playOn);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
